326a6640a6a53dcf8b1b3f2f83c93c7489f116c9
Amanda <amanda.v.barros6@aluno.senai.br>
2024-11-27 11:03:40 -0500

    commit com conteudo

---
+++ Notas.py
@@ -0,0 +1,10 @@
+nome = int(input("numero de alunos"))
+
+nota1 = float(input())
+nota2 = float(input())
+nota3 = float(input())
+nota4 = float(input())
+
+faltas = int(input("Numero de faltas:10"))
+media = (nota1 + nota2 + nota3 + nota4)/4
+print("o aluno esta de recuperação com a nota",media,"e com","faltas")
