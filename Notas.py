a6fb998df0d80def225dce022fec528034a3842d
Amanda <amanda.v.barros6@aluno.senai.br>
2024-11-28 10:59:45 -0500

    fazendo o codigo

--- Notas.py
+++ Notas.py
@@ -1,10 +1,38 @@
-nome = int(input("numero de alunos"))
 
+quantAluno = int(input("Digite a quantidade de alunos:"))
+
+quantFaltas = int(input("Digite  número de faltas:"))
+
+print("Digite o nome do aluno:")
+nome = (input())
+
+print("Digite a nota 1:")
 nota1 = float(input())
+print("Digite a nota 2:")
 nota2 = float(input())
+print("Digite a nota 3:")
 nota3 = float(input())
+print("Digite a nota 4:")
 nota4 = float(input())
 
-faltas = int(input("Numero de faltas:10"))
-media = (nota1 + nota2 + nota3 + nota4)/4
-print("o aluno esta de recuperação com a nota",media,"e com","faltas")
+media = (nota1 + nota2 + nota3 + nota4)/4 
+
+print("A média do", nome, "é:", media)
+
+
+if media >=8:
+   print("APROVADO")
+else:
+    print("REPROVADO por média")
+
+if (media >=5):
+   print("o aluno não precisa realizar a recuperação. Sua média é:",media)
+else:
+   print("REPROVADO, pois não foi permitido fazer a recuperação")
+
+
+
+
+
+
+
