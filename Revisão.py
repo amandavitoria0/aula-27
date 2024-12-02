a258ed6e1f019706c5d320bb6291c37dd3b10bf4
amanda <amanda.v.barros6@aluno.senai.br>
2024-12-02 10:16:46 -0500

    primeiro commit do dia 02/12

---
+++ Revisão.py
@@ -0,0 +1,15 @@
+#LER ENTRADAS DO USUÀRIO
+nome = input()#ARMAZENAR o nome do aluno
+nota1 = float(input())
+nota2 = float(input())
+nota3 = float(input())
+nota4 = float(input())
+ 
+faltas = int(input())
+#CALCULO DA MEDIA
+media = (nota1+nota2+nota3+nota4)/4
+print(media)
+
+
+
+
