ac318c29c602b0a552faa6f65a68d681b8c8ea21
amanda <amanda.v.barros6@aluno.senai.br>
2024-12-02 11:10:37 -0500

    primeiro commit do dia 02/12

--- Revisao.py
+++ Revisao.py
@@ -10,6 +10,27 @@ faltas = int(input())
 media = (nota1+nota2+nota3+nota4)/4
 print(media)
 
+#LÓGICA DA SITUAÇÃO
+if faltas > 31:
+    situacao = "Reprovado por falta"
+elif media >= 8:
+    situacao = "Aprovado"
+elif media >= 5: #RECUPERAÇÃO
+    recuperacao =float(input())#LER A NOTA DA PROVA DE RECUPERAÇÃO
+    if recuperacao >= (10-media):
+        situacao = "Aprovado na recuperação"
+    else:
+        situacao = "Reprovado na recuperação"
+else:
+    situacao = "Reprovado por média"
+
+    #RELATÓRIO
+print ("Nome:", nome)
+print ("Notas:",nota1, nota2, nota3, nota4)
+print("Faltas:",faltas)
+print("Média:",media)
+print("Recuperação:",)
+
 
 
 
