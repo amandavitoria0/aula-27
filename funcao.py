f615f27d7a5a0d50de0787537e485ef2279247df
Amanda <amanda.v.barros6@aluno.senai.br>
2024-12-11 08:47:58 -0500

    fazendo uma funcao

---
+++ funcao.py
@@ -0,0 +1,14 @@
+#DECLARAR UMA FUNÇÃO
+def bom_dia():
+    print("BOM DAI!")
+
+def boa_noite():
+    print("BUANAS NOCHES!")   
+
+def boa_tarde():
+     print("boa tarde!")
+#CHAMANDO AS FUNÇÕES
+bom_dia()
+boa_noite()
+boa_tarde()
+
